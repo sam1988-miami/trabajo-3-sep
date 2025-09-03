89b75ec72b3e13b696a67e0b52dbe68d30cd2d57
samuel <smanrique192@gmail.com>
2025-09-03 17:27:13 -0500

    parte final

--- 3-sep.py
+++ 3-sep.py
@@ -1,8 +1,10 @@
 
 print("1. Sumar")
 print("2. Restar")
+print("3. multiplicar")
+print("4. dividir")
 
-opcion = input("Elige una opción (1/2): ")
+opcion = input("Elige una opción (1/2/3/4): ")
 
 a = int(input("Ingrese el primer número: "))
 b = int(input("Ingrese el segundo número: "))
@@ -11,5 +13,9 @@ if opcion == "1":
     print(f"El resultado es = {a + b}")
 elif opcion == "2":
     print(f"El resultado es = {a - b}")
-else:
-    print("Opción no válida")
+if opcion == "3":
+     print(f"El resultado es = {a * b}")
+elif opcion == "4":
+    print(f"El resultado es = {a // b}")
+
+
